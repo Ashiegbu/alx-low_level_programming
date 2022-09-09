82d7b11da838d05ad67c2f7981320ef3d958cfcf
Ashiegbu <chibuch@gmail.com>
2022-09-09 11:57:56 -0700

    Print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,17 +6,17 @@
  */
 int main(void)
 {
-  int n;
+int n;
 
-  for (n = 48; n < 58; n++)
-    {
-      putchar(n);
-      if (n != 57)
-	{
-	  putchar(',');
-	  putchar(' ');
-	}
-    }
-  putchar('\n');
-  return (0);
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+if (n != 57)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
 }
