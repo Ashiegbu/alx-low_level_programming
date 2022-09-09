831d95ca700a2fb32021dc1ff73d163ea46c7161
Ashiegbu <chibuch@gmail.com>
2022-09-09 11:19:02 -0700

    Print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,14 +9,14 @@
  */
 int main(void)
 {
-  char c = '0';
+char c = '0';
 
-  while (c <= '9')
-    {
-      putchar(c);
-      c++;
-    }
+while (c <= '9')
+{
+putchar(c);
+c++;
+}
 
-  putchar('\n');
-  return (0);
+putchar('\n');
+return (0);
 }
