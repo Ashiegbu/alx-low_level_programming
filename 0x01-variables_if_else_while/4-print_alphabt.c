6f72fd2a0c248d0f1ef28c3336e89df4137bff06
Ashiegbu <chibuch@gmail.com>
2022-09-09 11:11:31 -0700

    Print alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,17 +6,17 @@
  */
 int main(void)
 {
-  char c = 'a';
+char c = 'a';
 
-  while (c <= 'z')
-    {
-      if (c != 'e' && c != 'q')
-	{
-	  putchar(c);
-	}
-      c++;
-    }
+while (c <= 'z')
+{
+if (c != 'e' && c != 'q')
+{
+putchar(c);
+}
+c++;
+}
 
-  putchar('\n');
-  return (0);
+putchar('\n');
+return (0);
 }
