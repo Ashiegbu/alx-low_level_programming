7b55bf0db0f4842b755b71d8c775480233112f1d
Ashiegbu <chibuch@gmail.com>
2022-09-09 11:50:35 -0700

    Print base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-  int n;
-  char ch;
+int n;
+char ch;
 
-  for (n = 48; n < 58; n++)
-    {
-      putchar(n);
-    }
-  for (ch = 'a'; ch <= 'f'; ch++)
-    {
-      putchar(ch);
-    }
-  putchar('\n');
-  return (0);
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+return (0);
 }
