09c843e7ffa6f84038fa60d6b004cac2f6587b07
Ashiegbu <chibuch@gmail.com>
2022-09-13 15:49:43 -0700

    Alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,11 +7,11 @@
  */
 void print_alphabet(void)
 {
-  int i;
+int i;
 
-  for (i = 97; i <= 122; i++)
-    {
-      _putchar(i);
-    }
-  _putchar('\n');
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
 }
