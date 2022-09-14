15d9dd63610de952dd1e3eea3916a09a48fd5b6d
Ashiegbu <chibuch@gmail.com>
2022-09-14 03:40:53 -0700

    isaplha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,16 +9,16 @@
 
 int _isalpha(int c)
 {
-  if (c >= 'a' && c <= 'z')
-    {
-      return (1);
-    }
-  else if (c >= 'A' && c <= 'Z')
-    {
-      return (1);
-    }
-  else
-    {
-      return (0);
-    }
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
