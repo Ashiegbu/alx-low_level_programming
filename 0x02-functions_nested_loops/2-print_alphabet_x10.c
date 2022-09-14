9f5245dfa45f313e3a914fbdf87afe5c0743abc3
Ashiegbu <chibuch@gmail.com>
2022-09-14 00:32:10 -0700

    Print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,19 +8,19 @@
 
 void print_alphabet_x10(void)
 {
-  int round = 0;
-  char letter = 'a';
+int round = 0;
+char letter = 'a';
 
-  while (round < 10)
-    {
-      letter = 'a';
-      while (letter <= 'z')
-	{
-	  _putchar(letter);
-	  letter++;
-	}
-      _putchar('\n');
+while (round < 10)
+{
+letter = 'a';
+while (letter <= 'z')
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
 
-      round++;
-    }
+round++;
+}
 }
