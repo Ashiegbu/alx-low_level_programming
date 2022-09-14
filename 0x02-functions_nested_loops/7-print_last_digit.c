fcd23cce1f13a26a139ae51aff14b9b4f48990a4
Ashiegbu <chibuch@gmail.com>
2022-09-14 04:03:11 -0700

    Print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,12 +11,12 @@
 
 int print_last_digit(int n)
 {
-  int last = n % 10;
+int last = n % 10;
 
-  if (n < 0)
-    last = last * -1;
+if (n < 0)
+last = last * -1;
 
-  _putchar(last + '0');
+_putchar(last + '0');
 
-  return (last);
+return (last);
 }
