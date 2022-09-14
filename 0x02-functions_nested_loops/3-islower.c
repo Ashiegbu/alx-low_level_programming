f982bf88c5ba3bae4d9541ba8b669cca94797f19
Ashiegbu <chibuch@gmail.com>
2022-09-14 02:23:54 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -9,12 +9,12 @@
 
 int _islower(int c)
 {
-  if (c >= 'a' && c <= 'z')
-    {
-      return (1);
-    }
-  else
-    {
-      return (0);
-    }
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
