f37d0681e2a69762b7ad6e2c6e819578c21199fd
Ashiegbu <chibuch@gmail.com>
2022-09-14 03:55:49 -0700

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -9,16 +9,16 @@
 
 int _abs(int n)
 {
-  if (n > 0)
-    {
-      return (n);
-    }
-  else if (n < 0)
-    {
-      return (-n);
-    }
-  else
-    {
-      return (n);
-    }
+if (n > 0)
+{
+return (n);
+}
+else if (n < 0)
+{
+return (-n);
+}
+else
+{
+return (n);
+}
 }
