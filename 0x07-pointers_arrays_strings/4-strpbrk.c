77e7f86ca25a5103411e564803c063f98c604641
Ashiegbu <chibuch@gmail.com>
2022-09-26 01:03:59 -0700

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _strpbrk - matches any character specified
- * @s: This is the C string to be scanned.
- * @accept:character in str1 that matches one of the characters in str2
- *Return: string s that matches any character specified in accept
- **/
+* _strpbrk - matches any character specified
+* @s: This is the C string to be scanned.
+* @accept:character in str1 that matches one of the characters in str2
+*Return: string s that matches any character specified in accept
+**/
 
 char *_strpbrk(char *s, char *accept)
 {
